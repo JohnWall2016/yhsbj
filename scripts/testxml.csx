8a1738f788fecd697d61e011bdea7cdf7faaaa1b
wj <wj>
2018-07-04 16:03:53 +0800

    update

--- scripts/testxml.csx
+++ scripts/testxml.csx
@@ -1,167 +1,8 @@
 #! "netcoreapp2.0"
 #r "nuget: YAXLib, 2.15.0"
-#r "nuget: System.Xml.Linq, 3.5.21022.801"
+#r "../src/YHSBJ.SBGLPT/bin/Debug/netstandard2.0/YHSBJ.SBGLPT.dll"
 
-using System.Linq;
-using System.Xml.Linq;
-using YAXLib;
-
-public interface ICustomSerializable
-{
-    string ToXAttributeValue();
-    XElement ToXElement();
-    string ToValue();
-    void LoadXAttribute(XAttribute attrib);
-    void LoadXElement(XElement element);
-    void LoadValue(string value);
-}
-
-public class BaseCustomSerializable : ICustomSerializable
-{
-    public virtual string ToXAttributeValue()
-    {
-        throw new NotImplementedException();
-    }
-    public virtual XElement ToXElement()
-    {
-        throw new NotImplementedException();
-    }
-    public virtual string ToValue()
-    {
-        throw new NotImplementedException();
-    }
-    public virtual void LoadXAttribute(XAttribute attrib)
-    {
-        throw new NotImplementedException();
-    }
-    public virtual void LoadXElement(XElement element)
-    {
-        throw new NotImplementedException();
-    }
-    public virtual void LoadValue(string value)
-    {
-        throw new NotImplementedException();
-    }
-}
-
-public class CustomSerializer<T> : ICustomSerializer<T> where T : ICustomSerializable, new()
-{
-    public void SerializeToAttribute(T serializable, XAttribute attrToFill)
-    {
-        attrToFill.Value = serializable.ToXAttributeValue();
-    }
-
-    public void SerializeToElement(T serializable, XElement elemToFill)
-    {
-        elemToFill.Add(serializable.ToXElement());
-    }
-
-    public string SerializeToValue(T serializable)
-    {
-        return serializable.ToValue();
-    }
-
-    public T DeserializeFromAttribute(XAttribute attrib)
-    {
-        var serializable = new T();
-        serializable.LoadXAttribute(attrib);
-        return serializable;
-    }
-
-    public T DeserializeFromElement(XElement element)
-    {
-        var serializable = new T();
-        serializable.LoadXElement(element);
-        return serializable;
-    }
-
-    public T DeserializeFromValue(string value)
-    {
-        var serializable = new T();
-        serializable.LoadValue(value);
-        return serializable;
-    }
-}
-
-[YAXNamespace("soap", "http://schemas.xmlsoap.org/soap/envelope/")]
-public class Envelope<TCustom>
-{
-    [YAXAttributeForClass()]
-    public string encodingStyle { get; set; } = "http://schemas.xmlsoap.org/soap/encoding/";
-    
-    public TCustom Header { get; set; }
-
-    public TCustom Body { get; set; }
-
-    public override string ToString()
-    {
-        YAXSerializer serializer = new YAXSerializer(typeof(Envelope<TCustom>));
-        var doc = serializer.SerializeToXDocument(this);
-        doc.Root.ReplaceAttributes(doc.Root.Attributes()
-                                   .OrderByDescending(attr => attr.Name.Namespace.NamespaceName));
-        return "<?xml version=\"1.0\" encoding=\"GBK\"?>" +
-            doc.ToString(SaveOptions.DisableFormatting).Replace(" />", "/>");
-    }
-
-    public static Envelope<TCustom> Load(string xml)
-    {
-       YAXSerializer serializer = new YAXSerializer(typeof(Envelope<TCustom>));
-       return (Envelope<TCustom>)serializer.Deserialize(xml);
-    }
-}
-
-[YAXCustomSerializer(typeof(CustomSerializer<Input>))]
-public class Input : BaseCustomSerializable
-{
-    public static XNamespace Namespace => "http://www.molss.gov.cn/";
-    public static string NamespacePrefix => "in";
-
-    public string Name { get; private set; }
-    public Dictionary<string, object> Params { get; } = new Dictionary<string, object>();
-
-    public Input(string name)
-    {
-        Name = name;
-    }
-
-    public Input() : this("") {}
-    
-    public override XElement ToXElement()
-    {
-        var elem = new XElement(Namespace + Name);
-        elem.SetAttributeValue(XNamespace.Xmlns + NamespacePrefix, Namespace);
-
-        if (Params.Count == 0)
-            elem.Value = "";
-        else
-        {
-            foreach (var (k, v) in Params)
-            {
-                var para = new XElement("para");
-                para.SetAttributeValue(k, v?.ToString() ?? "");
-                elem.Add(para);
-            }
-        }
-        return elem;
-    }
-
-    public override void LoadXElement(XElement element)
-    {
-        var elem = element.Elements().First(e => e.Name.Namespace == Namespace);
-        if (elem != null)
-        {
-            this.Name = elem.Name.LocalName;
-            foreach (var el in elem.Elements("para"))
-            {
-                foreach (var attr in el.Attributes())
-                {
-                    this.Params.Add(attr.Name.LocalName, attr.Value);
-                }
-            }
-        }
-        return;
-    }
-}
+using YHSBJ.SBGLPT;
 
 var env = new Envelope<Input>
 {
@@ -181,3 +22,27 @@ foreach (var (k, v) in env.Header.Params)
 {
     Console.WriteLine("{0}:{1}", k, v);
 }
+
+var outEnv = Envelope<Output>.Load(
+@"<?xml version=""1.0"" encoding=""GBK""?><soap:Envelope xmlns:soap=""http://schemas.xmlsoap.org/soap/envelope/"" soap:encodingStyle=""http://schemas.xmlsoap.org/soap/encoding/""><soap:Header><result sessionID=""hpyzb7GLwxhWmPrd9JMbD9K8rsPp2DTKV8TYnJ11FsQ5zTjxW1QP!1220643430!1530594344225""/><result message=""查询同步数据库信息成功""/></soap:Header><soap:Body><out:business xmlns:out=""http://www.molss.gov.cn/""><result versionid="""" /><result result=""查询同步数据库信息成功"" /><resultset name=""outtable""><row versionid=""45067"" enabled=""1"" controlname=""B06.04.02"" parentid=""M01.02"" functiondesc=""网上经办灵活就业人员在职停保"" imageid=""0"" sstype=""100"" flag=""1"" helpid=""0"" functionid=""B06.04.02"" bplname=""dagl.bpl"" menuindex=""102414"" comments="""" classid="""" /><row versionid=""45068"" enabled=""1"" controlname=""B06.04.03"" parentid=""M01.02"" functiondesc=""网上经办灵活就业人员在职续保"" imageid=""0"" sstype=""100"" flag=""1"" helpid=""0"" functionid=""B06.04.03"" bplname=""dagl.bpl"" menuindex=""102413"" comments="""" classid="""" /></resultset></out:business></soap:Body></soap:Envelope>"                               
+                               );
+
+void printDictList(List<Dictionary<string, object>> list)
+{
+    foreach (var e in list)
+    {
+        foreach (var (k, v) in e)
+            Console.Write("{0}:{1}|", k, v);
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("header:{0}", outEnv.Header.Name);
+printDictList(outEnv.Header.Results);
+Console.WriteLine("header.resultset:{0}", outEnv.Header.Resultset.Name);
+printDictList(outEnv.Header.Resultset.Rows);
+
+Console.WriteLine("body:{0}", outEnv.Body.Name);
+printDictList(outEnv.Body.Results);
+Console.WriteLine("header.resultset:{0}", outEnv.Body.Resultset.Name);
+printDictList(outEnv.Body.Resultset.Rows);
--- src/YHSBJ.SBGLPT/Transactions.cs
+++ src/YHSBJ.SBGLPT/Transactions.cs
@@ -1,76 +1,227 @@
 ﻿using System;
 using System.Linq;
 using System.Xml.Linq;
+using System.Collections.Generic;
 using YAXLib;
 
 namespace YHSBJ.SBGLPT
 {
+    public interface ICustomSerializable
+    {
+        void ToXAttribute(XAttribute attrToFill);
+        void ToXElement(XElement elemToFill);
+        string ToValue();
+        void LoadXAttribute(XAttribute attrib);
+        void LoadXElement(XElement element);
+        void LoadValue(string value);
+    }
+
+    public class BaseCustomSerializable : ICustomSerializable
+    {
+        public virtual void ToXAttribute(XAttribute attrToFill)
+        {
+            throw new NotImplementedException();
+        }
+        public virtual void ToXElement(XElement elemToFill)
+        {
+            throw new NotImplementedException();
+        }
+        public virtual string ToValue()
+        {
+            throw new NotImplementedException();
+        }
+        public virtual void LoadXAttribute(XAttribute attrib)
+        {
+            throw new NotImplementedException();
+        }
+        public virtual void LoadXElement(XElement element)
+        {
+            throw new NotImplementedException();
+        }
+        public virtual void LoadValue(string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CustomSerializer<T> : ICustomSerializer<T> where T : ICustomSerializable, new()
+    {
+        public void SerializeToAttribute(T serializable, XAttribute attrToFill)
+        {
+            serializable.ToXAttribute(attrToFill);
+        }
+
+        public void SerializeToElement(T serializable, XElement elemToFill)
+        {
+            serializable.ToXElement(elemToFill);
+        }
+
+        public string SerializeToValue(T serializable)
+        {
+            return serializable.ToValue();
+        }
+
+        public T DeserializeFromAttribute(XAttribute attrib)
+        {
+            var serializable = new T();
+            serializable.LoadXAttribute(attrib);
+            return serializable;
+        }
+
+        public T DeserializeFromElement(XElement element)
+        {
+            var serializable = new T();
+            serializable.LoadXElement(element);
+            return serializable;
+        }
+
+        public T DeserializeFromValue(string value)
+        {
+            var serializable = new T();
+            serializable.LoadValue(value);
+            return serializable;
+        }
+    }
+
+    [YAXSerializeAs("Envelope")]
     [YAXNamespace("soap", "http://schemas.xmlsoap.org/soap/envelope/")]
-    public class Envelope
+    public class Envelope<TCustom>
     {
         [YAXAttributeForClass()]
-        public string encodingStyle => "http://schemas.xmlsoap.org/soap/encoding/";
+        public string encodingStyle { get; set; } = "http://schemas.xmlsoap.org/soap/encoding/";
+    
+        public TCustom Header { get; set; }
 
-        public Header Header => new Header();
+        public TCustom Body { get; set; }
+
+        public override string ToString()
+        {
+            YAXSerializer serializer = new YAXSerializer(typeof(Envelope<TCustom>));
+            var doc = serializer.SerializeToXDocument(this);
+            doc.Root.ReplaceAttributes(doc.Root.Attributes()
+                                       .OrderByDescending(attr => attr.Name.Namespace.NamespaceName));
+            return "<?xml version=\"1.0\" encoding=\"GBK\"?>" +
+                doc.ToString(SaveOptions.DisableFormatting).Replace(" />", "/>");
+        }
+
+        public static Envelope<TCustom> Load(string xml)
+        {
+            YAXSerializer serializer = new YAXSerializer(typeof(Envelope<TCustom>));
+            return (Envelope<TCustom>)serializer.Deserialize(xml);
+        }
     }
 
-    public class Header
+    [YAXCustomSerializer(typeof(CustomSerializer<Input>))]
+    public class Input : BaseCustomSerializable
     {
-        public System system => new System();
+        public static XNamespace Namespace => "http://www.molss.gov.cn/";
+        public static string NamespacePrefix => "in";
+
+        public string Name { get; private set; }
+        public Dictionary<string, object> Params { get; } = new Dictionary<string, object>();
+
+        public Input(string name)
+        {
+            Name = name;
+        }
+
+        public Input() : this("") {}
+    
+        public override void ToXElement(XElement elemToFill)
+        {
+            var elem = new XElement(Namespace + Name);
+            elem.SetAttributeValue(XNamespace.Xmlns + NamespacePrefix, Namespace);
+
+            if (Params.Count == 0)
+                elem.Value = "";
+            else
+            {
+                foreach (var kv in Params)
+                {
+                    var para = new XElement("para");
+                    para.SetAttributeValue(kv.Key, kv.Value?.ToString() ?? "");
+                    elem.Add(para);
+                }
+            }
+            elemToFill.Add(elem);
+        }
+
+        public override void LoadXElement(XElement element)
+        {
+            var elems = element.Elements().Where(e => e.Name.Namespace == Namespace);
+            if (elems.Any())
+            {
+                var elem = elems.ElementAt(0);
+                this.Name = elem.Name.LocalName;
+                foreach (var el in elem.Elements("para"))
+                {
+                    foreach (var attr in el.Attributes())
+                    {
+                        this.Params.Add(attr.Name.LocalName, attr.Value);
+                    }
+                }
+            }
+        }
     }
 
-    public class Para
+    [YAXCustomSerializer(typeof(CustomSerializer<Output>))]
+    public class Output : BaseCustomSerializable
     {
-        public string usr { get; set; }
-        public string pwd { get; set; }
-        public string funid { get; set; }
+        public static XNamespace Namespace => "http://www.molss.gov.cn/";
+        public static string NamespacePrefix => "out";
+
+        public string Name { get; private set; }
+        public List<Dictionary<string, object>> Results { get; set; } = new List<Dictionary<string, object>>();
+        public ResultSet Resultset { get; set; } = new ResultSet();
+
+        public class ResultSet
+        {
+            public string Name { get; set; }
+            public List<Dictionary<string, object>> Rows = new List<Dictionary<string, object>>();
+        }
+
+        public Output(string name)
+        {
+            Name = name;
+        }
+
+        public Output() : this("") {}
+    
+        public override void LoadXElement(XElement element)
+        {
+            XElement elem = null;
+            var elems = element.Elements().Where(e =>e.Name.Namespace == Namespace);
+            if (elems.Any())
+            {
+                elem = elems.ElementAt(0);
+                this.Name = elem.Name.LocalName;
+            }
+            else
+            {
+                elem = element;
+                this.Name = "";
+            }
+
+            void populate(List<Dictionary<string, object>> list, XElement xelem, string selectName)
+            {
+                foreach (var el in xelem.Elements(selectName))
+                {
+                    var dict = new Dictionary<string, object>();
+                    foreach (var attr in el.Attributes())
+                        dict.Add(attr.Name.LocalName, attr.Value);
+                    list.Add(dict);
+                }
+            }
+
+            populate(this.Results, elem, "result");
+
+            elems = elem.Elements("resultset");
+            if (elems.Any())
+            {
+                var rset = elems.ElementAt(0);
+                this.Resultset.Name = rset.Attribute("name")?.Value;
+                populate(this.Resultset.Rows, rset, "row");
+            }
+        }
     }
-
-    [YAXCustomSerializer(typeof(CustomSystemSerializer))]
-    public class System
-    {
-        public Para para => new Para();
-    }
-
-    public class CustomSystemSerializer : ICustomSerializer<System>
-    {
-        public void SerializeToAttribute(System objectToSerialize, XAttribute attrToFill)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SerializeToElement(System objectToSerialize, XElement elemToFill)
-        {
-            //string message = objectToSerialize.MessageText;
-            //string beforeBold = message.Substring(0, objectToSerialize.BoldIndex);
-            //string afterBold = message.Substring(objectToSerialize.BoldIndex + objectToSerialize.BoldLength);
-            //
-            //elemToFill.Add(new XText(beforeBold));
-            //elemToFill.Add(new XElement("b", objectToSerialize.BoldContent));
-            //elemToFill.Add(new XText(afterBold));
-            throw new NotImplementedException();
-        }
-
-        public string SerializeToValue(System objectToSerialize)
-        {
-            throw new NotImplementedException();
-        }
-
-        public System DeserializeFromAttribute(XAttribute attrib)
-        {
-            throw new NotImplementedException();
-        }
-
-        public System DeserializeFromElement(XElement element)
-        {
-            throw new NotImplementedException();
-        }
-
-        public System DeserializeFromValue(string value)
-        {
-            throw new NotImplementedException();
-        }
-
-    }
-
 }
